112f5371fb1a342a83bb1fdd74a785fbd463b6f4
Joe Isabell <joeisabell@me.com>
2017-06-22 18:41:56 -0600

    added major setup pieces of relay

---
+++ src/Environment.js
@@ -0,0 +1,26 @@
+const { Environment, Network, RecordSource, Store } = require('relay-runtime')
+
+const store = new Store(new RecordSource())
+
+const network = Network.create((operation, variables) => {
+  return fetch('https://api.graph.cool/relay/v1/cj48yk52jjwmp0141ljw9q7i2', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+})
+
+const environment = new Environment({
+  network,
+  store,
+})
+
+export default environment
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { QueryRenderer, graphql } from 'react-relay';
+import environment from './../../Environment';
 
 import './App.css';
 
 import Navbar from './Navbar/Navbar';
 import Dashboard from './Dashboard/Dashboard';
 
+
+const AppAllDataQuery = graphql`
+  query AppAllDataQuery {
+    viewer {
+      ...Dashboard_viewer
+    }
+  }
+`
+
 class App extends Component {
   render() {
     return (
-      <div className="App__Dashboard">
-        <Navbar />
-        <Switch>
-          <Route path="/app" component={ Dashboard } />
-        </Switch>
-      </div>
+      <QueryRenderer
+        environment={ environment }
+        query={ AppAllDataQuery }
+        render={ ({ error, props }) => {
+            if(error) {
+              return <div>error.message</div>
+            } else if (props) {
+              return (
+                <div className="App__Dashboard">
+                  <Navbar />
+                  <Dashboard viewer={ props.viewer } />
+                </div>
+              )
+            }
+        }}
+      />
     );
   }
 }
